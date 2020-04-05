155b00368db496f156e13a07573901f138ad7db7
Ethan <esadowski1@my.bcit.ca>
2020-04-05 13:36:34 -0700

    Added storage to Google Sheet

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ const express = require("express")
 const app = express()
 const ejsLayouts = require("express-ejs-layouts")
 const reminderController = require("./controllers/reminder_controller");
-
+const fs = require('fs');
+const readline = require('readline');
+const {google} = require('googleapis');
+const http = require('http');
+const SCOPES = ['https://www.googleapis.com/auth/spreadsheets'];
+const TOKEN_PATH = 'token.json';
+let Database = require("./database");
 
 app.use(express.static(__dirname + "/public"))
 
@@ -35,3 +41,93 @@ app.post("/reminder/delete/:id", reminderController.delete)
 app.listen(3000, function(){
   console.log("Server running. Visit: localhost:3000/reminder in your browser 🚀");
 })
+
+
+
+fs.readFile('credentials.json', (err, content) => {
+  if (err) return console.log('Error loading client secret file:', err);
+  authorize(JSON.parse(content), listReminders);
+});
+
+/**
+ * Create an OAuth2 client with the given credentials, and then execute the given callback function.
+ * @param {Object} credentials The authorization client credentials.
+ * @param {function} callback The callback to call with the authorized client.
+ */
+function authorize(credentials, callback) {
+  const {client_secret, client_id, redirect_uris} = credentials.installed;
+  const oAuth2Client = new google.auth.OAuth2(
+      client_id, client_secret, redirect_uris[0]);
+
+  // Check if we have previously stored a token.
+  fs.readFile(TOKEN_PATH, (err, token) => {
+    if (err) return getNewToken(oAuth2Client, callback);
+    oAuth2Client.setCredentials(JSON.parse(token));
+    callback(oAuth2Client);
+  });
+}
+
+/**
+ * Get and store new token after prompting for user authorization, and then
+ * execute the given callback with the authorized OAuth2 client.
+ * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
+ * @param {getEventsCallback} callback The callback for the authorized client.
+ */
+function getNewToken(oAuth2Client, callback) {
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: SCOPES,
+  });
+  console.log('Authorize this app by visiting this url:', authUrl);
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  rl.question('Enter the code from that page here: ', (code) => {
+    rl.close();
+    oAuth2Client.getToken(code, (err, token) => {
+      if (err) return console.error('Error while trying to retrieve access token', err);
+      oAuth2Client.setCredentials(token);
+      // Store the token to disk for later program executions
+      fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+        if (err) return console.error(err);
+        console.log('Token stored to', TOKEN_PATH);
+      });
+      callback(oAuth2Client);
+    });
+  });
+}
+
+/*
+ * 
+ * @param {google.auth.OAuth2} auth The authenticated Google OAuth client.
+ */
+function listReminders(auth) {
+  const sheets = google.sheets({version: 'v4', auth});
+  sheets.spreadsheets.values.get({
+    spreadsheetId: '1l8XiLrVRqbjaBzmKJBmi4aMxKqlJdV6b_XT_mTH0vAQ',
+    range: 'Sheet1',
+  }, (err, res) => {
+    if (err) return console.log('The API returned an error: ' + err);
+      let data = res.data.values;
+    if (data == undefined){
+      return console.log('No data to return');
+    }
+    if (data.length) {
+      for (i = 0; i < data.length; i++){
+        if (data[i][0] != undefined) {
+          newReminder = {
+            id: data[i][0],
+            title: data[i][1],
+            date: data[i][2],
+            description: data[i][3],
+            rain: bool = data[i][4] == "true",
+            subtasks: [data[i][5], data[0][6], data[0][7], data[0][8]],
+            completed: bool = data[i][9] == "true",
+          }
+          Database.cindy.reminders.push(newReminder);
+        }
+      }
+    }
+  });
+}
